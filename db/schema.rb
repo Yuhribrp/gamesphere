67e8d02a32981c4b6ca39fcf11c63ffd86cf836a
Juliana Arrighi <jarrighi@poli.ufrj.br>
2021-08-31 11:19:46 -0300

    comment model done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_141623) do
+ActiveRecord::Schema.define(version: 2021_08_31_141849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "like"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "communities", force: :cascade do |t|
     t.string "title"
@@ -72,6 +81,7 @@ ActiveRecord::Schema.define(version: 2021_08_31_141623) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "communities", "posts"
   add_foreign_key "evaluations", "users"
   add_foreign_key "memberships", "communities"
