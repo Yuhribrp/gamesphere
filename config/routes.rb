46ff8c73300c702f2a34afdb8a22533dcf69bac7
Juliana Arrighi <jarrighi@poli.ufrj.br>
2021-08-31 16:52:15 -0300

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :create, :show] do
-    resources :evaluations, only: [:index, :create]
-    resources :friendship, only: [:create]
-  end
+  # resources :users, only: [:index, :create, :show]
+    # resources :evaluations, only: [:index, :create]
+    # resources :friendship, only: [:create]
+    # end
   resources :communities, only: [:index, :show] do
-    resources :posts, only: [:post]
+    resources :posts, only: [:new, :create]
     resources :match, only: [:create]
   end
 end
