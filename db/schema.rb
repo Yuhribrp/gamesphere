b43a6a51eefbb43f3b608c66efbb5cd3a24481bc
Juliana Arrighi <jarrighi@poli.ufrj.br>
2021-08-31 16:10:10 -0300

    fixing models

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_141849) do
+ActiveRecord::Schema.define(version: 2021_08_31_190833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comments", force: :cascade do |t|
-    t.text "content"
-    t.integer "like"
-    t.bigint "post_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-  end
-
   create_table "communities", force: :cascade do |t|
     t.string "title"
-    t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_communities_on_post_id"
   end
 
   create_table "evaluations", force: :cascade do |t|
@@ -45,6 +34,16 @@ ActiveRecord::Schema.define(version: 2021_08_31_141849) do
     t.index ["user_id"], name: "index_evaluations_on_user_id"
   end
 
+  create_table "gametags", force: :cascade do |t|
+    t.string "game_title"
+    t.integer "raking"
+    t.string "skil_level"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_gametags_on_user_id"
+  end
+
   create_table "memberships", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "community_id", null: false
@@ -52,16 +51,6 @@ ActiveRecord::Schema.define(version: 2021_08_31_141849) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["community_id"], name: "index_memberships_on_community_id"
     t.index ["user_id"], name: "index_memberships_on_user_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "photo"
-    t.string "video"
-    t.text "content"
-    t.string "title"
-    t.integer "likes"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -81,9 +70,8 @@ ActiveRecord::Schema.define(version: 2021_08_31_141849) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "communities", "posts"
   add_foreign_key "evaluations", "users"
+  add_foreign_key "gametags", "users"
   add_foreign_key "memberships", "communities"
   add_foreign_key "memberships", "users"
 end
