c914a13dd2191450e4e8689f2cc03192fb199ea6
Juliana Arrighi <jarrighi@poli.ufrj.br>
2021-08-31 11:17:23 -0300

    membership model done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_140423) do
+ActiveRecord::Schema.define(version: 2021_08_31_141623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 2021_08_31_140423) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_evaluations_on_user_id"
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "community_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["community_id"], name: "index_memberships_on_community_id"
+    t.index ["user_id"], name: "index_memberships_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -65,4 +74,6 @@ ActiveRecord::Schema.define(version: 2021_08_31_140423) do
 
   add_foreign_key "communities", "posts"
   add_foreign_key "evaluations", "users"
+  add_foreign_key "memberships", "communities"
+  add_foreign_key "memberships", "users"
 end
