9beb19b1e6df3484a58e504fc402d1b25fa9109d
Justin Knox <techbiotic@posteo.de>
2021-09-01 15:41:23 -0300

    added in to model

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,4 +1,11 @@
 class Community < ApplicationRecord
   has_many :posts
   has_many :users, through: :memberships
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title,
+    against: [ :title ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
