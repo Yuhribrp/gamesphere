a013d7d98a3837d5e4773b15455b56b0787d2c4d
Justin Knox <techbiotic@posteo.de>
2021-09-03 11:59:21 -0300

    first six communities are now matched with the hompage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_01_172204) do
+ActiveRecord::Schema.define(version: 2021_09_02_224738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 2021_09_01_172204) do
     t.string "language"
     t.string "location"
     t.integer "age"
+    t.string "avatar_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
