29fa46f26be7d83b775fde6a679d01de12123377
Justin Knox <techbiotic@posteo.de>
2021-09-07 10:55:22 -0300

    fixed links on communities page

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -2,24 +2,26 @@ class CommunitiesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home ]
 
   def index
-    # @search = params["search"]
-    # if @search.present?
-    #   @title = @search["title"]
-    #   @communities = Community.where(title: @title)
-    # end
+    @search = params["search"]
+    if @search.present?
+      @title = @search["title"]
+      @communities = Community.where(title: @title)
+    end
     if params[:query].present?
       @communities = Community.search_by_title(params[:query])
     else
       @communities = Community.all
     end
   end
-  # def posts
-  #   @community = Community.find[params[:id]]
-  #   @post = Post.new
-  # end
+
+  def posts
+    @community = Community.find[params[:id]]
+    @post = Post.new
+  end
 
   def show
-    @community = Self.find_community
+    # @community = Community.find_community
+    @community = Community.find[params[:id]]
     # @post = Post.new
   end
 
