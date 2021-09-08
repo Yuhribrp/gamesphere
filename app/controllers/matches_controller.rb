dc39e3312e74e48e22fb386334008faf53a80339
Juliana Arrighi <jarrighi@poli.ufrj.br>
2021-09-08 15:34:09 -0300

    matches table access done

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,6 +2,7 @@ class MatchesController < ApplicationController
   def index
     @player_one = current_user
     @player_two = User.where(age: current_user.age, language: current_user.language, location: current_user.location).sample
+    @matches = Match.where(user_one_id: current_user.id) || Match.where(user_two_id: current_user.id)
   end
 
   def create
@@ -11,9 +12,12 @@ class MatchesController < ApplicationController
     if @player_one != @player_two
       @match = Match.new(user_one_id: @player_one.id, user_two_id: @player_two.id)
       @match.save
+      #@matches << @match
       redirect_to community_matches_path(@community)
     else
-      flash[:alert] = "erro"
+      flash[:alert] = "No new matches for now!"
     end
   end
 end
+
+# User.where(user_id: match.user_one_id).username
