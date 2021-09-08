b6efc2eb7698c9dcc5ea6868d20d028ddf1d4f47
Juliana Arrighi <jarrighi@poli.ufrj.br>
2021-09-08 11:33:07 -0300

    match redirected to index, no modal anymore

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,25 +1,20 @@
 class MatchesController < ApplicationController
-
   def index
     @player_one = current_user
     @player_two = User.where(age: current_user.age, language: current_user.language, location: current_user.location).sample
-    # if @player_one != @player_two
-    #   @player_two
-    # else
-    #   # no matches today
-    #   redirect_to :action => "index", :id => @community
-    # end
   end
 
   def create
+    @community = Community.find(params[:community_id])
+    @player_one = current_user
+    @player_two = User.where(age: current_user.age, language: current_user.language, location: current_user.location).sample
     if @player_one != @player_two
-      @match = Match.find(params[:match_id])
-      @match = Match.new(@player_one[:id], @player_two[:id])
-      if @match.save
-        puts "ahhhhhhhh"
-      end
+      @match = Match.new(@player_one, @player_two)
+    end
+    if @match.save
+      redirect_to communities_matches_path(@community)
     else
-      redirect_to communities_path
+      flash[:alert] = "erro"
     end
   end
 end
