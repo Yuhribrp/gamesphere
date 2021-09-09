c2a3d1026e64b44f57f5f10471d2a0777ed7413e
Juliana Arrighi <jarrighi@poli.ufrj.br>
2021-09-09 12:14:02 -0300

    matches with css

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -11,22 +11,11 @@ class MatchesController < ApplicationController
     @player_two = User.where(age: current_user.age, language: current_user.language, location: current_user.location).sample
     @combination = [@player_one, @player_two].combination(2).to_a
     @matches = Match.where(user_one_id: current_user.id) || Match.where(user_two_id: current_user.id)
-
-
-    if @matches == [ ]
+    if @matches == [] || @player_one != @player_two && @matches.include?(@combination)
       @match = Match.new(user_one_id: @player_one.id, user_two_id: @player_two.id)
-
       @match.save
-      redirect_to community_matches_path(@community)
-    else
-      if @player_one != @player_two && @matches.include?(@combination)
-        @match = Match.new(user_one_id: @player_one.id, user_two_id: @player_two.id)
-        @match.save
-        redirect_to community_matches_path(@community)
-      else
-        redirect_to community_matches_path(@community)
-        flash[:alert] = "No new matches for now!"
-      end
+      # flash[:alert] = "No new matches for now!"
     end
+    redirect_to community_matches_path(@community)
   end
 end
